e2d59e327ff342aa23c7ca3a8eb99c8a6b41cdf8
Alex Antonio Silva <axsilva74@gmail.com>
2024-07-31 00:11:57 -0300

    Ajustes gerais

--- inicializaChat.js
+++ inicializaChat.js
@@ -1,8 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv, { parse } from 'dotenv';
        dotenv.config();
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); //('AIzaSyDloHGRP5hndljL4IgM8V4BFp49eu4bqI0');
+
+const funcoes = {
+  taxaJurosDeLicenca: ({ value }) => {
+    const tamanhoPropriedade = typeof value === "string" ? parseInt(value) : value;
+    if (tamanhoPropriedade <= 4) {
+      return 120;
+    } else if (tamanhoPropriedade <= 10) {
+      return 180;
+    } else if (tamanhoPropriedade <= 20) {
+      return 230;
+    }
+  }
+};
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
 
