f65cc74d0fde373192467dba56efcd4c945852df
Alex Antonio Silva <axsilva74@gmail.com>
2024-07-31 03:54:08 -0300

    Vercel arquivo 8

--- static/js/app.js
+++ static/js/app.js
@@ -25,7 +25,7 @@ async function enviarMensagem() {
         const response = await fetch('https://chatbotsecagri.vercel.app/chat', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({'mensagem': mensagem})
         }); 
