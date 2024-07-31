eaff4f0cf628b5e1838f04cc399efc60d72db8cb
Alex Antonio Silva <axsilva74@gmail.com>
2024-07-31 00:27:17 -0300

    Ajustes gerais3

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,7 @@ async function enviarMensagem() {
     input.value = '';
 
     try {
-        const response = await fetch('http://chatbotsecagri.vercel.app/chat', {
+        const response = await fetch('https://chatbotsecagri.vercel.app/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
